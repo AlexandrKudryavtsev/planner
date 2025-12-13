1919c4667624416df063a6e030ea24a20aad1427
your Name <qwertyshurazh@gmail.com>
2025-12-13 08:04:01 +0300

    Updates

--- _next/static/VXdV5UWH2j0EyIsIunBpy/_buildManifest.js
+++ _next/static/VXdV5UWH2j0EyIsIunBpy/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/planner//_next/:path+",
-        "destination": "/planner/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
